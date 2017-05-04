09a5049f9fd8df487ca87322ea6260f0c882a766
MJL7068 <mattil@netti.fi>
2017-05-04 12:02:37 +0300

    testejä lisätty

--- src/test/java/miniproju/controllers/IndexControllerTest.java
+++ src/test/java/miniproju/controllers/IndexControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,17 +16,18 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
  * @author matleino
  */
 public class IndexControllerTest {
-    private MockMvc mockMvc;
+//    private MockMvc mockMvc;
 
-    @Mock
-    private ReferenceServiceJpaImpl refService;
+//    @Mock
+//    private ReferenceServiceJpaImpl refService;
 
     // injects mock or spy fields into tested objects automatically
-    @InjectMocks
-    private IndexController indexController;
+//    @InjectMocks
+//    private IndexController indexController;
 
     @Before
     public void setUp() {
+//        MockitoAnnotations.initMocks(this);
 //        mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
     }
 
@@ -33,8 +35,7 @@ public class IndexControllerTest {
     @Test
     public void indexIsRendered() throws Exception {
 //        mockMvc.perform(get("/"))
-//                .andExpect(status().isOk())
-//                /*.andExpect(view().name("/"))*/;
+//                .andExpect(status().isOk());
     }
     
 
--- src/test/java/miniproju/models/ReferenceTest.java
+++ src/test/java/miniproju/models/ReferenceTest.java
@@ -25,6 +25,65 @@ public class ReferenceTest {
     }
     
     @Test
+    public void gettersAndSettersWork() {
+        Reference r = new Reference("Test");
+        r.setEntryType("Article");
+        r.setEntryKey("123");
+        r.setAuthor("Allan Collins and John Seely Brown and Ann Holum");
+        r.setJournal("American Educator");
+        r.setYear(1991);
+        r.setVolume("6");
+        r.setPages("38-46");
+        r.setId(Long.MIN_VALUE);
+        r.setAddress("Osoite");
+        r.setAnnote("annote");
+        r.setBooktitle("Book title");
+        r.setChapter("3");
+        r.setCrossref("Cross ref");
+        r.setEdition("22");
+        r.setEditor("Editor");
+        r.setHowpublished("Pub");
+        r.setInstitution("institution");
+        r.setKey("2");
+        r.setMonth("12");
+        r.setNote("Note");
+        r.setNumber("123");
+        r.setOrganization("organization");
+        r.setPublisher("Publisher");
+        r.setSchool("School");
+        r.setSeries("7");
+        r.setType("Type");
+        
+        assertEquals("Test", r.getTitle());
+        assertEquals("Article", r.getEntryType());
+        assertEquals("123", r.getEntryKey());
+        assertEquals("Allan Collins and John Seely Brown and Ann Holum", r.getAuthor());
+        assertEquals("American Educator", r.getJournal());
+        assertEquals(1991, r.getYear());
+        assertEquals("6", r.getVolume());
+        assertEquals("38-46", r.getPages());
+        assertEquals("" + Long.MIN_VALUE, "" + r.getId());
+        assertEquals("Osoite", r.getAddress());
+        assertEquals("annote", r.getAnnote());
+        assertEquals("Book title", r.getBooktitle());
+        assertEquals("3", r.getChapter());
+        assertEquals("Cross ref", r.getCrossref());
+        assertEquals("22", r.getEdition());
+        assertEquals("Editor", r.getEditor());
+        assertEquals("Pub", r.getHowpublished());
+        assertEquals("institution", r.getInstitution());
+        assertEquals("2", r.getKey());
+        assertEquals("12", r.getMonth());
+        assertEquals("Note", r.getNote());
+        assertEquals("123", r.getNumber());
+        assertEquals("organization", r.getOrganization());
+        assertEquals("Publisher", r.getPublisher());
+        assertEquals("School", r.getSchool());
+        assertEquals("7", r.getSeries());
+        assertEquals("Type", r.getType());
+    }
+    
+    @Test
     public void toBibtexStringReturnsCorrectString() {
         String v = "@Article{123,\nauthor = {{A}llan {C}ollins and {J}ohn {S}eely {B}rown and {A}nn {H}olum},\njournal = {{A}merican {E}ducator},\npages = {38-46},\ntitle = {{C}ognitive apprenticeship: making thinking visible},\nvolume = {6},\nyear = {1991}\n}";
         assertEquals(v, ref.toBibtexString());
